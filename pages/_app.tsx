147df99a43546b6fffc9d3d6fe838db3806e8269
Leabs <stevenleabo@gmail.com>
2022-07-13 10:52:42 -0400

    Adding wallet connection support with 3rdweb

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
+
+import "regenerator-runtime/runtime";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const supportedChainIds = [80001, 4];
+
+  const connectors = {
+    injected: {},
+  };
+
+  return (
+    <ThirdwebWeb3Provider
+      supportedChainIds={supportedChainIds}
+      connectors={connectors}
+    >
+      <Component {...pageProps} />
+    </ThirdwebWeb3Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
